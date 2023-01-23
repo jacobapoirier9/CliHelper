07d7d861b887a6cda78dccf8db909f99be0514b3
Jacob Poirier <jacobapoirier9@gmail.com>
2023-01-23 16:50:48 -0500

    move configuration to its own file

--- CliHelper/CliClient.cs
+++ CliHelper/CliClient.cs
@@ -7,20 +7,6 @@ using System.Reflection;
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 
-internal static class Configuration
-{
-    public static readonly Type[] SimpleTypes = new Type[]
-    {
-        typeof(string),
-
-        typeof(bool), typeof(char), typeof(DateTime), typeof(TimeSpan),
-        typeof(bool?), typeof(char?), typeof(DateTime?), typeof(TimeSpan?),
-
-        typeof(byte), typeof(short), typeof(int), typeof(long), typeof(float), typeof(double), typeof(decimal),
-        typeof(byte?), typeof(short?), typeof(int?), typeof(long?), typeof(float?), typeof(double?), typeof(decimal?),
-    };
-}
-
 public sealed class CliClient
 {
     /// <summary>
